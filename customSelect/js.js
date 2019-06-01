f2665dca86f1d8fceebe6b3cd6d97b1f498c1b52
KapishDima <kapishdima@gmail.com>
2019-06-01 12:48:48 +0300

    commit

--- customSelect/js.js
+++ customSelect/js.js
@@ -4,101 +4,90 @@ var optionsWrapper,
     optionsItem,
     selectButton;
 
-console.log(selection);
+function Select(className){
 
+    this.select = selection[0].children[0].getElementsByClassName("s1");
+    function createSelect(select){
+        var mainSelectWrapper;
+        mainSelectWrapper = document.createElement("div");
+        mainSelectWrapper.setAttribute("class", "main-select-wrapper");
 
-function createOptionsWrapper(select){
-    optionsWrapper = document.createElement("div");
-    optionsWrapper.setAttribute("class", "options-wrapper hide");
-    for(var i = 0; i < select[0].options.length; i++){
-        optionsItem = document.createElement("div");
-        optionsItem.setAttribute("class", "option");
-        optionsItem.innerHTML = select[0].options[i].innerHTML;
-        optionsWrapper.appendChild(optionsItem);
-        optionsItem.addEventListener("click", function(e){
-            // e.stopPropagation();
+        mainSelectWrapper.appendChild(createSelectButton(select));
+        mainSelectWrapper.appendChild(createOptionsWrapper(select));
 
-            var s = this.parentNode.parentNode.parentNode.getElementsByClassName("selects")[0].getElementsByTagName("select")[0];
-            var sB = this.parentNode.parentNode.getElementsByClassName("select-button")[0];
+        selection[0].appendChild(mainSelectWrapper);
 
-            for(var i = 0; i < s.length; i++){
-                if(s.options[i].innerHTML == this.innerHTML){
-                    s.selectedIndex = i;
-                    sB.innerHTML = this.innerHTML;
-                    
-                    var selectedOption = this.parentNode.getElementsByClassName("selected-option");
-                    for (j = 0; j < selectedOption.length; j++) {
-                        selectedOption[j].classList.remove("selected-option");
+    }
+
+    function createSelectButton (select){
+        selectButton = document.createElement("div");
+        selectButton.setAttribute("class", "select-button");
+        selectButton.innerHTML = select[0].options[select[0].selectedIndex].innerHTML;
+        selectButton.addEventListener("click", function(e){    
+            e.stopPropagation();
+            closeSelect(select);
+            this.nextSibling.classList.toggle("hide");
+        });
+
+        return selectButton;
+    }
+
+    function createOptionsWrapper(select){
+        optionsWrapper = document.createElement("div");
+        optionsWrapper.setAttribute("class", "options-wrapper hide");
+        for(var i = 0; i < select[0].options.length; i++){
+            optionsItem = document.createElement("div");
+            optionsItem.setAttribute("class", "option");
+            optionsItem.innerHTML = select[0].options[i].innerHTML;
+            optionsWrapper.appendChild(optionsItem);
+            optionsItem.addEventListener("click", function(e){
+                e.stopPropagation();
+
+                var s = this.parentNode.parentNode.parentNode.getElementsByClassName("selects")[0].getElementsByTagName("select")[0];
+                var sB = this.parentNode.parentNode.getElementsByClassName("select-button")[0];
+
+                for(var i = 0; i < s.length; i++){
+                    if(s.options[i].innerHTML == this.innerHTML){
+                        s.selectedIndex = i;
+                        sB.innerHTML = this.innerHTML;
+                        
+                        var selectedOption = this.parentNode.getElementsByClassName("selected-option");
+                        for (j = 0; j < selectedOption.length; j++) {
+                            selectedOption[j].classList.remove("selected-option");
+                        }
+                        this.classList.add("selected-option");
+                        break;
                     }
-                    this.classList.add("selected-option");
-                    break;
+                }
+                sB.click();
+            });
+        }
+
+        return optionsWrapper;
+    }
+    
+    function closeSelect(select) {
+        document.addEventListener("click", function(){
+            var dOptionWrapper = [];
+            
+            for(var i = 0; i < select[0].parentNode.parentNode.getElementsByClassName("main-select-wrapper").length; i++){
+                dOptionWrapper.push(select[0].parentNode.parentNode.getElementsByClassName("main-select-wrapper")[i].getElementsByClassName("options-wrapper")[0]);
+            }
+    
+            for(var i = 0; i < dOptionWrapper.length; i++){
+                if(!dOptionWrapper[i].classList.contains("hide")){
+                    dOptionWrapper[i].classList.add("hide");
                 }
             }
-            sB.click();
+    
         });
     }
 
-    return optionsWrapper;
-
-}
-
-function closeSelect(select){
-    document.addEventListener("click", function(){
-        var dOptionWrapper = [];
-        
-        for(var i = 0; i < select[0].parentNode.parentNode.getElementsByClassName("main-select-wrapper").length; i++){
-            dOptionWrapper.push( select[0].parentNode.parentNode.getElementsByClassName("main-select-wrapper")[i].getElementsByClassName("options-wrapper")[0]);
-        }
-        console.log(dOptionWrapper);
-
-        for(var i = 0; i < dOptionWrapper.length; i++){
-            if(!dOptionWrapper[i].classList.contains("hide")){
-                console.log("true");
-                dOptionWrapper[i].classList.add("hide");
-            }
-        }
-
-    });
-}
-
-function createSelectButton(select){
-    selectButton = document.createElement("div");
-    selectButton.setAttribute("class", "select-button");
-    selectButton.innerHTML = select[0].options[select[0].selectedIndex].innerHTML;
-    console.log(selectButton);
-    selectButton.addEventListener("click", function(e){    
-        e.stopPropagation();
-        closeSelect(select);
-        this.nextSibling.classList.toggle("hide");
-    });
-
-    return selectButton;
-}
-
-function createSelect(select){
-    var mainSelectWrapper;
-    mainSelectWrapper = document.createElement("div");
-    mainSelectWrapper.setAttribute("class", "main-select-wrapper");
-
-    mainSelectWrapper.appendChild(createSelectButton(select));
-    mainSelectWrapper.appendChild(createOptionsWrapper(select));
-
-    selection[0].appendChild(mainSelectWrapper);
-
-}
-
-function Select(select){
-
-    this.select = select;
-    console.log(this.select);
-    this.constrCreateSelect = function(){
-        createSelect(this.select);
-    }
-    
+    createSelect(this.select);
 }
 
 
-var select1 = new Select(selection[0].children[0].getElementsByClassName("s1")).constrCreateSelect();
-var select2 = new Select(selection[0].children[0].getElementsByClassName("s2")).constrCreateSelect();
-var select3 = new Select(selection[0].children[0].getElementsByClassName("s")).constrCreateSelect();
-var select4 = new Select(selection[0].children[0].getElementsByClassName("s3")).constrCreateSelect();
+var select1 = new Select("s1");
+var select2 = new Select("s2");
+var select3 = new Select("s3");
+var select4 = new Select("s");
